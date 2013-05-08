a55b0bdb04bdbd024e1183b1f5c94f3c9c097d04
Valeri Colov <valeri.colov@gmail.com>
2013-05-08 09:04:52 +0300

    JSPart2

---
+++ JS-Part2/01.DOMManipulationHW/01.RandomDivs/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("_01.RandomDivs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("_01.RandomDivs")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8f7c287d-38a1-41c9-afc5-a1b97912a04d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ JS-Part2/03.OOPHW/01.Vehicles/Vehicles.js
@@ -0,0 +1,220 @@
+﻿function Vehicle() {
+    var self = this;
+    var speed;
+    var propulsionUnit;
+    var numberOfPropulsionUnits = 0;
+
+    self.SetSpeed = function (value) {
+        speed = value;
+    }
+
+    self.GetSpeed = function () {
+        return speed;
+    }
+
+    self.SetPropulsionUnit = function (value) {
+        propulsionUnit = value;
+    }
+
+    self.GetPropulsionUnit = function () {
+        return propulsionUnit;
+    }
+
+    self.SetNumberOfPropulsionUnit = function (value) {
+        numberOfPropulsionUnits = value;
+    }
+
+    self.GetNumberOfPropulstionUnits = function () {
+        return numberOfPropulsionUnits;
+    }
+}
+function LandVehicle(numberOfPropulsionUnits,radius) {
+    Vehicle.call(this);
+    var propulsionUnit = this.SetPropulsionUnit(new Wheel(radius));
+    this.SetNumberOfPropulsionUnit(numberOfPropulsionUnits);
+
+    
+    this.Accelerate = function () {
+        this.SetSpeed(this.GetSpeed() + this.GetPropulsionUnit().GetAcc());
+    }
+}
+LandVehicle.prototype = new Vehicle();
+LandVehicle.prototype.constructor = LandVehicle;
+
+function AirVehicle(numberOfPropulsionUnits) {
+    Vehicle.call(this);
+    var self = this;
+    var propulsionUnit = self.SetPropulsionUnit(new PropellNozzle(250));
+    self.AfterBurnersOn = function (state) {
+        if (state != self.GetPropulsionUnit().GetAfterBurnerState()) {
+            self.GetPropulsionUnit().SetAfterBurnerState(state);
+        }
+    }
+    self.Accelerate = function () {
+        self.SetSpeed(self.GetSpeed() + self.GetPropulsionUnit().GetAcc() * self.GetNumberOfPropulstionUnits());
+    }
+
+    self.SetNumberOfPropulsionUnit(numberOfPropulsionUnits);
+}
+AirVehicle.prototype = new Vehicle();
+AirVehicle.prototype.constructor = AirVehicle;
+
+function WaterVehicle(numberOfPropulsionUnits) {
+    Vehicle.call(this);
+    var self = this;
+    var propulsionUnit;
+    self.SetPropulsionUnit(new Propeller("clockwise"));
+
+    self.ChangeSpinDirection = function (newDir) {
+        self.GetPropulsionUnit().SetSpinDir(newDir);
+    }
+
+    self.GetSpinDirection = function () {
+        if (self.GetPropulsionUnit().GetSpinDir()) {
+            return "clockwise";
+        } else {
+            return "counter-clockwise";
+        }
+    }
+
+    self.Accelerate = function () {
+        self.SetSpeed(self.GetSpeed() + self.GetPropulsionUnit().GetAcc() * self.GetNumberOfPropulstionUnits());
+    }
+
+    self.SetNumberOfPropulsionUnit(numberOfPropulsionUnits);
+}
+WaterVehicle.prototype = new Vehicle();
+WaterVehicle.prototype.constructor = WaterVehicle;
+
+function Amphibius(numberOfPropulsionUnits) {
+    Vehicle.call(this);
+    var self = this;
+    var mode = "land";
+    self.SetNumberOfPropulsionUnit(numberOfPropulsionUnits);
+
+    self.GetMode = function () {
+        return mode;
+    }
+
+    self.SetMode = function (value) {
+        mode = value;
+    }
+
+    self.ChangeMode = function (newMode) {
+        if (newMode === "water" || newMode === "land") {
+            if (newMode !== self.GetMode()) {
+                self.SetMode(newMode);
+                self.SetPropUnitDependingOnMode();
+            } else {
+                console.log("   Now you are in the wanted mode");
+            }
+        } else {
+            console.log("   There is no such mode");
+        }
+    }
+
+    self.SetPropUnitDependingOnMode = function () {
+        self.SetNumberOfPropulsionUnit(numberOfPropulsionUnits);
+        if (self.GetMode() === "water") {            
+            self.SetPropulsionUnit(new Propeller("clockwise"));
+        } else if (self.GetMode() === "land") {
+            self.SetPropulsionUnit(new Wheel(3));
+        }
+    }
+
+    self.SetPropulsionUnit(self.SetPropUnitDependingOnMode());
+
+    self.ChangeSpinDirection = function (newDir) {
+        if (self.GetPropulsionUnit() instanceof Propeller) {
+            self.GetPropulsionUnit().SetSpinDir(newDir);
+        } else {
+            console.log("   You are in a land mode.Fans are disabled");
+        }
+    }
+
+    self.Accelerate = function () {
+        self.SetSpeed(self.GetSpeed() + self.GetPropulsionUnit().GetAcc() * numberOfPropulsionUnits);
+    }
+}
+Amphibius.prototype = new Vehicle();
+Amphibius.prototype.constructor = Amphibius;
+
+//I am not sure why i need this class, but i think that through it i reach good abstraction which i can use in Vehicle class
+function PropulsionUnit() {
+    this.GetAcc = function () {
+    }
+}
+
+function Wheel(radius) {
+    PropulsionUnit.call(this);
+
+    var radius = radius;
+
+    this.GetAcc = function () {
+        return 2 * Math.PI * radius;
+    }
+}
+Wheel.prototype = new PropulsionUnit();
+Wheel.prototype.constructor = Wheel;
+
+function PropellNozzle(power) {
+    PropulsionUnit.call(this);
+    var self = this;
+    var power = power;
+    var afterBurner = false;
+
+    self.SetAfterBurnerState = function (state) {
+        afterBurner = state;
+    }
+
+    self.GetAfterBurnerState = function () {
+        return afterBurner;
+    }
+
+    self.GetAcc = function () {
+        if (self.GetAfterBurnerState()) {
+            return 2 * power;
+            }
+        return power;
+        }
+}
+PropellNozzle.prototype = new PropulsionUnit();
+PropellNozzle.prototype.constructor = PropellNozzle;
+
+function Propeller(spinDirection) {
+    PropulsionUnit.call(this);
+    var self = this;
+    var numberOfFans = 2;
+    var spinDirection;
+    
+
+    self.SetNumberOfFans = function (value) {
+        numberOfFans = value;
+    }
+
+    self.GetNumberOfFans = function () {
+        return numberOfFans;
+    }
+
+    self.SetSpinDir = function (newDir) {
+        if (newDir === "clockwise") {
+            spinDirection = true;
+        } else if (newDir === "counter-clockwise") {
+            spinDirection = false;
+        }
+    }
+
+    self.SetSpinDir(spinDirection);
+    self.GetSpinDir = function () {
+        return spinDirection;
+    }
+
+    self.GetAcc = function () {
+        if (self.GetSpinDir()) {
+            return self.GetNumberOfFans();
+        }
+        return -self.GetNumberOfFans();
+    }
+}
+Propeller.prototype = new PropulsionUnit();
+Propeller.prototype.constructor = Propeller;
